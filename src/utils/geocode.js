1b5e389c7e07612e7c706fcba1375c21b2cd95c9
shivanshu5416jais <42492030+shivanshu5416jais@users.noreply.github.com>
2020-04-29 18:23:47 +0530

    Add about text

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -3,7 +3,7 @@ const request=require('request')
 const geocode=(address, callback)=>{
 
     const url='https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1Ijoic2p0ZWNrZXJzIiwiYSI6ImNrOWJoNGNyaDAzejczZnF2eGs5Ym81MnQifQ.sQcLh54Ecqv00VXnI0ce_A&limit=1'
-    request({url,json:true},(error,{body})=>{
+    request({url,json:true},(error,{body}={})=>{
         if(error){
             callback('Unable to connect to location services!',undefined)
         }
